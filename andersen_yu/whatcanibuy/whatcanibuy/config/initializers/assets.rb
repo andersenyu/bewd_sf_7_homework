938dc0ac8a60c894b2616ac606bed0b1daafdb40
Andersen Yu <andersenyu0716@gmail.com>
2015-01-23 17:15:35 -0800

    fix welcome page add nav bar and mute button

--- andersen_yu/whatcanibuy/whatcanibuy/config/initializers/assets.rb
+++ andersen_yu/whatcanibuy/whatcanibuy/config/initializers/assets.rb
@@ -6,3 +6,12 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( welcomepage.js )
+Rails.application.config.assets.precompile += %w( bootstrap.js )
+Rails.application.config.assets.precompile += %w( ct-navbar.js )
+Rails.application.config.assets.precompile += %w( jquery-1.10.2.js )
+
+# Rails.application.config.assets.precompile += %w( bootstrap.scss )
+# Rails.application.config.assets.precompile += %w( ct-navbar.scss )
+# Rails.application.config.assets.precompile += %w( jquery-1.10.2.scss )
+# Rails.application.config.assets.precompile += %w( pe-icon-7-stroke.scss )
